8f75ff271913cc3894d78706fb9790e25637ec05
apogee23 <tate.l.hayes@gmail.com>
2022-07-03 16:22:24 -1000

    add DEVELOPMENT_MODE setting

--- api_proxy/settings.py
+++ api_proxy/settings.py
@@ -75,6 +75,7 @@ WSGI_APPLICATION = 'api_proxy.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
+DEVELOPMENT_MODE = os.getenv("DEVELOPMENT_MODE", "False") == "True"
 
 if DEVELOPMENT_MODE is True:
     DATABASES = {
@@ -125,7 +126,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = "/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
