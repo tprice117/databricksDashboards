9c73c6050ae5702ce1a8826cb1797ca7dd16ce22
apogee23 <tate.l.hayes@gmail.com>
2023-02-06 22:54:08 -0600

    add static directory

--- api_proxy/settings.py
+++ api_proxy/settings.py
@@ -166,6 +166,11 @@ USE_TZ = True
 STATIC_URL = "/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
+# Add these new lines
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
