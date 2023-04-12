84f613277510910055cd28809111f3cee400fe08
apogee23 <tate.l.hayes@gmail.com>
2023-04-12 14:57:56 -0500

    add main product name to add ons

--- api/models.py
+++ api/models.py
@@ -219,7 +219,7 @@ class ProductAddOnChoice(BaseModel):
     add_on_choice = models.ForeignKey(AddOnChoice, models.DO_NOTHING)
 
     def __str__(self):
-        return self.name
+        return f'{self.product.main_product.name} - {self.add_on_choice.add_on.name} - {self.add_on_choice.name}'
 
 class WasteType(BaseModel):
     name = models.CharField(max_length=80)
