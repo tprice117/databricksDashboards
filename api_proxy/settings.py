532ae6e64bc034091b7b85416a324c57e8c0d256
apogee23 <tate.l.hayes@gmail.com>
2023-04-25 17:12:57 -0500

    add COERCE_DECIMAL_TO_STRING false

--- api_proxy/settings.py
+++ api_proxy/settings.py
@@ -153,6 +153,7 @@ REST_FRAMEWORK = {
     ),
     'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
+    'COERCE_DECIMAL_TO_STRING': False,
 }
 
 if ENVIRONMENT == 'TEST':
