8202dc32d373d04c8aac47054b699ea457577b66
apogee23 <tate.l.hayes@gmail.com>
2023-06-01 23:11:21 +0200

    add disposal location to the orderDisposalTicket inline

--- api/admin.py
+++ api/admin.py
@@ -78,7 +78,7 @@ class UserGroupUserInline(admin.TabularInline):
 
 class OrderDisposalTicketInline(admin.TabularInline):
     model = OrderDisposalTicket
-    fields = ('ticket_id', 'waste_type', 'weight')
+    fields = ('ticket_id', 'disposal_location', 'waste_type', 'weight')
     show_change_link = True
     extra=0
 
