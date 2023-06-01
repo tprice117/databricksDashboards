dd01744f88adff9d8153ebd493d8b68276dd9c44
apogee23 <tate.l.hayes@gmail.com>
2023-06-01 15:41:51 +0200

    remove COERCE_DECIMAL_TO_STRING

--- api_proxy/settings.py
+++ api_proxy/settings.py
@@ -173,7 +173,7 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.BasicAuthentication',
     ),
     'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
-    'COERCE_DECIMAL_TO_STRING': False
+    # 'COERCE_DECIMAL_TO_STRING': False
 }
 
 if ENVIRONMENT == 'TEST':
