532b60a5b8175eef5996fc81c2c5492208c9fd2d
apogee23 <tate.l.hayes@gmail.com>
2023-06-07 16:53:42 +0200

    add is_admin to User

--- api/models.py
+++ api/models.py
@@ -137,6 +137,7 @@ class User(BaseModel):
     first_name = models.CharField(max_length=255, blank=True, null=True)
     last_name = models.CharField(max_length=255, blank=True, null=True)
     device_token= models.CharField(max_length=255, blank=True, null=True)
+    is_admin = models.BooleanField(default=False)
 
     def __str__(self):
         return self.email
