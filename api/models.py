dc4dd19e042529273f81ef80420bfd4ae60dac0f
apogee23 <tate.l.hayes@gmail.com>
2023-06-13 01:41:37 +0200

    handle stringify error

--- api/models.py
+++ api/models.py
@@ -305,7 +305,7 @@ class OrderGroup(BaseModel):
     subscription = models.ForeignKey(Subscription, models.PROTECT, blank=True, null=True)
 
     def __str__(self):
-        return f'{self.user.user_group.name} - {self.user.email} - {self.seller_product_seller_location.seller_location.seller.name}'
+        return f'{self.user.user_group.name if self.user.user_group else ""} - {self.user.email} - {self.seller_product_seller_location.seller_location.seller.name}'
 
 class ProductAddOnChoice(BaseModel):
     name = models.CharField(max_length=80)
