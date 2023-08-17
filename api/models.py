46dfb95c489285996729839f69ad20cd1c59dcb9
apogee23 <tate.l.hayes@gmail.com>
2023-08-17 14:38:39 -0500

    add title to UserSellerReview

--- api/models.py
+++ api/models.py
@@ -239,6 +239,7 @@ class UserSellerLocation(BaseModel):
 class UserSellerReview(BaseModel): #added this model 2/25/2023 by Dylan
     seller = models.ForeignKey(Seller, models.DO_NOTHING, related_name='user_seller_review')
     user = models.ForeignKey(User, models.DO_NOTHING, related_name='user_seller_review')
+    title = models.CharField(max_length=255)
     rating = models.IntegerField(blank=True, null=True)
     comment = models.TextField(blank=True, null=True)
     
