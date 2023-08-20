23c00966d6da61890e20d970cc1e5460e6b28e40
apogee23 <tate.l.hayes@gmail.com>
2023-08-20 15:38:17 -0500

    remove tonnage included and ass sellerproduct active field

--- api/models.py
+++ api/models.py
@@ -343,6 +343,7 @@ class Product(BaseModel):
 class SellerProduct(BaseModel):
     product = models.ForeignKey(Product, models.CASCADE, related_name='seller_products')
     seller = models.ForeignKey(Seller, models.CASCADE, related_name='seller_products')
+    active = models.BooleanField(default=True)
    
     def __str__(self):
         return self.product.main_product.name + ' - ' + (self.product.product_code or "") + ' - ' + self.seller.name
