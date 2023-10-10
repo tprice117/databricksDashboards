4cce36719f9ac48357bf11ab19b94e6a8959bb27
apogee23 <tate.l.hayes@gmail.com>
2023-10-10 15:19:30 -0500

    turn off decimal to string

--- api_proxy/settings.py
+++ api_proxy/settings.py
@@ -158,7 +158,7 @@ REST_FRAMEWORK = {
     ),
     'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
-    # 'COERCE_DECIMAL_TO_STRING': False
+    'COERCE_DECIMAL_TO_STRING': False
 }
 
 if ENVIRONMENT == 'TEST':
