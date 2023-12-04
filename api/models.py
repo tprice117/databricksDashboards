fb205600b87e3700b043b68c77312880c0375a0c
apogee23 <tate.l.hayes@gmail.com>
2023-12-04 13:58:25 -0600

    add stripe_tax_code_id

--- api/models.py
+++ api/models.py
@@ -943,8 +943,9 @@ class OrderDisposalTicket(BaseModel):
     
 class OrderLineItemType(BaseModel):
     name = models.CharField(max_length=255)
-    units = models.CharField(max_length=255, blank=True, null=True)
-    code = models.CharField(max_length=255, blank=True, null=True)
+    units = models.CharField(max_length=255)
+    code = models.CharField(max_length=255)
+    stripe_tax_code_id = models.CharField(max_length=255)
 
     def __str__(self):
         return self.name
