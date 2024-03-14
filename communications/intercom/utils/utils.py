f8d3e5a374b9ba7b9fdcd6425101699ec9e986bd
wickeym <wickeym@gmail.com>
2024-03-14 15:09:53 -0400

    UPDATE: Add Response type to Intercom api return.

--- communications/intercom/utils/utils.py
+++ communications/intercom/utils/utils.py
@@ -1,8 +1,20 @@
 import requests
+from dataclasses import dataclass
 from django.conf import settings
+
+# Intercom API typings
+
+
+@dataclass
+class IntercomResponse():
+    data: any
+    # https://developers.intercom.com/docs/references/rest-api/errors/http-responses/
+    status_code: int
 
 
 class IntercomUtils:
+    """Refer to REST api docs: https://developers.intercom.com/docs/references/introduction/
+    """
     headers = {
         "Authorization": f"Bearer {settings.INTERCOM_ACCESS_TOKEN}",
     }
@@ -11,15 +23,22 @@ class IntercomUtils:
     def create(
         endpoint: str,
         data: dict,
-    ):
+    ) -> IntercomResponse:
+        """Create an item in an Intercom endpoint.
+
+        Args:
+            endpoint (str): Endpoint to update (contacts, companies, etc.).
+            data (dict): Dict of data to create the item.
+
+        Returns:
+            IntercomResponse: Response will contain status_code and data, which is the api json.
         """
-        Create an item in an Intercom endpoint.
-        """
-        return requests.post(
+        response = requests.post(
             f"https://api.intercom.io/{endpoint}",
             headers=IntercomUtils.headers,
             json=data,
-        ).json()
+        )
+        return IntercomResponse(data=response.json(), status_code=response.status_code)
 
     @staticmethod
     def get_all(
@@ -59,39 +78,49 @@ class IntercomUtils:
     def get(
         endpoint: str,
         item_id: str,
-    ):
+    ) -> IntercomResponse:
         """
         Get an item from an Intercom endpoint by its ID.
         """
-        return requests.get(
+        response = requests.get(
             f"https://api.intercom.io/{endpoint}/{item_id}",
             headers=IntercomUtils.headers,
-        ).json()
+        )
+        return IntercomResponse(data=response.json(), status_code=response.status_code)
 
     @staticmethod
     def update(
         endpoint: str,
         item_id: str,
         data: dict,
-    ):
+    ) -> IntercomResponse:
+        """Update an item from an Intercom endpoint.
+
+        Args:
+            endpoint (str): Endpoint to update (contacts, companies, etc.).
+            item_id (str): Id of the item to update (contact_id, company_id, etc.).
+            data (dict): Dict of data to update.
+
+        Returns:
+            IntercomResponse: Response will contain status_code and data, which is the api json.
         """
-        Update an item from an Intercom endpoint.
-        """
-        return requests.put(
+        response = requests.put(
             f"https://api.intercom.io/{endpoint}/{item_id}",
             headers=IntercomUtils.headers,
             json=data,
-        ).json()
+        )
+        return IntercomResponse(data=response.json(), status_code=response.status_code)
 
     @staticmethod
     def delete(
         endpoint: str,
         item_id: str,
-    ):
+    ) -> IntercomResponse:
         """
         Delete an item from an Intercom endpoint.
         """
-        return requests.delete(
+        response = requests.delete(
             f"https://api.intercom.io/{endpoint}/{item_id}",
             headers=IntercomUtils.headers,
-        ).json()
+        )
+        return IntercomResponse(data=response.json(), status_code=response.status_code)
