ede0aa4566440e6672f085919b219dde9b02a24b
wickeym <wickeym@gmail.com>
2024-04-04 15:38:01 -0400

    UPDATE: Add lob_check_id to Payout db model

--- api/models/payout.py
+++ api/models/payout.py
@@ -7,5 +7,6 @@ class Payout(BaseModel):
     order = models.ForeignKey("api.Order", models.CASCADE, related_name="payouts")
     checkbook_payout_id = models.CharField(max_length=255, blank=True, null=True)
     stripe_transfer_id = models.CharField(max_length=255, blank=True, null=True)
+    lob_check_id = models.CharField(max_length=255, blank=True, null=True)
     amount = models.DecimalField(max_digits=18, decimal_places=2)
     description = models.CharField(max_length=255, blank=True, null=True)
