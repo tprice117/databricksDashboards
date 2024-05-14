c62c70243cb5d7fb44cb5abc6c140050425a610e
wickeym <wickeym@gmail.com>
2024-05-14 15:12:09 -0400

    UPDATE: Add pagination to locations

--- supplier_dashboard/views.py
+++ supplier_dashboard/views.py
@@ -900,10 +900,37 @@ def locations(request):
     context = {}
     # context["user"] = request.user
     context["seller"] = get_seller(request)
+    pagination_limit = 25
+    page_number = 1
+    if request.GET.get("p", None) is not None:
+        page_number = request.GET.get("p")
+    # This is an HTMX request, so respond with html snippet
+    # if request.headers.get("HX-Request"):
+    query_params = request.GET.copy()
     seller_locations = SellerLocation.objects.filter(
         seller_id=request.session["seller"]["id"]
     )
-    context["seller_locations"] = seller_locations
+    paginator = Paginator(seller_locations, pagination_limit)
+    page_obj = paginator.get_page(page_number)
+    context["page_obj"] = page_obj
+
+    if page_number is None:
+        page_number = 1
+    else:
+        page_number = int(page_number)
+
+    query_params["p"] = 1
+    context["page_start_link"] = f"/supplier/locations/?{query_params.urlencode()}"
+    query_params["p"] = page_number
+    context["page_current_link"] = f"/supplier/locations/?{query_params.urlencode()}"
+    if page_obj.has_previous():
+        query_params["p"] = page_obj.previous_page_number()
+        context["page_prev_link"] = f"/supplier/locations/?{query_params.urlencode()}"
+    if page_obj.has_next():
+        query_params["p"] = page_obj.next_page_number()
+        context["page_next_link"] = f"/supplier/locations/?{query_params.urlencode()}"
+    query_params["p"] = paginator.num_pages
+    context["page_end_link"] = f"/supplier/locations/?{query_params.urlencode()}"
     return render(request, "supplier_dashboard/locations.html", context)
 
 
