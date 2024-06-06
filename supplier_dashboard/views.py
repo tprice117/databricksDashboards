429ea804b403dad4f44b03d38b34a7aaf20e560a
wickeym <wickeym@gmail.com>
2024-06-06 16:14:39 -0400

    FIX: Go back only 1 year in supplier dashboard graph

--- supplier_dashboard/views.py
+++ supplier_dashboard/views.py
@@ -88,9 +88,9 @@ def get_dashboard_chart_data(data_by_month: List[int]):
     ]
     current_month = datetime.date.today().month
     months = []
-    for i in range(8, 1, -1):
-        months.append(all_months[(current_month - i - 1) % 12])
-        data.append(data_by_month[(current_month - i - 1) % 12])
+    for i in range(11, 0, -1):
+        months.append(all_months[(current_month - i) % 12])
+        data.append(round(data_by_month[(current_month - i) % 12], 2))
 
     dashboard_chart = {
         "type": "line",
@@ -293,9 +293,13 @@ def index(request):
         complete_count = 0
         cancelled_count = 0
         earnings_by_month = [0] * 12
+        one_year_ago = datetime.date.today() - datetime.timedelta(days=365)
         for order in orders:
             context["earnings"] += float(order.seller_price())
-            earnings_by_month[order.end_date.month - 1] += float(order.seller_price())
+            if order.end_date >= one_year_ago:
+                earnings_by_month[order.end_date.month - 1] += float(
+                    order.seller_price()
+                )
 
             category = (
                 order.order_group.seller_product_seller_location.seller_product.product.main_product.main_product_category.name
