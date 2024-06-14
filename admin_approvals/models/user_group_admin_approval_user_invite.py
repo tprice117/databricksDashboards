cdb0c9b92948558422c2dc2cc9a6953bbc13f451
apogee23 <tate.l.hayes@gmail.com>
2024-06-14 08:28:23 -0500

    update UserGroupAdminApprovalUserInvite pre_save logic

--- admin_approvals/models/user_group_admin_approval_user_invite.py
+++ admin_approvals/models/user_group_admin_approval_user_invite.py
@@ -1,6 +1,8 @@
 import logging
 
 from django.db import models
+from django.db.models.signals import pre_save
+from django.dispatch import receiver
 from django.template.loader import render_to_string
 
 from api.models.track_data import track_data
@@ -59,55 +61,74 @@ class UserGroupAdminApprovalUserInvite(BaseModel):
     def __str__(self):
         return f"{self.user_group.name} - {self.email}"
 
-    def save(self, *args, **kwargs):
-        """Saves the object to the db if the created_by user is an ADMIN or if the UserGroup has a policy that requires approval.
-        Otherwise the User is created and this object is not saved to the db."""
-        old_status = self.old_value("status")
-        # https://docs.djangoproject.com/en/4.2/ref/models/instances/#django.db.models.Model._state
-        if self._state.adding is True:
-            # Object just created
-            if self.created_by.type == "ADMIN":
-                self.status = ApprovalStatus.APPROVED
-            elif hasattr(self.user_group, "policy_invitation_approvals"):
-                policy = self.user_group.policy_invitation_approvals.filter(
-                    user_type=self.created_by.type
+
+@receiver(pre_save, sender=UserGroupAdminApprovalUserInvite)
+def pre_save_user_group_admin_approval_order(
+    sender, instance: UserGroupAdminApprovalUserInvite, **kwargs
+):
+    """
+    Saves the object to the db if the created_by user is an ADMIN or if the UserGroup has a policy that requires approval.
+    Otherwise the User is created and this object is not saved to the db.
+    """
+    old_status = instance.old_value("status")
+
+    # If old_status is not PENDING, throw an error.
+    if not old_status == ApprovalStatus.PENDING:
+        raise ValueError(
+            f"UserGroupAdminApprovalUserInvite cannot be updated once the Status is not PENDING. Current Status: {old_status}"
+        )
+    else:
+        # The UserGroupAdminApprovalUserInvite.Status is PENDING. It's
+        # either being created or updated.
+
+        if instance._state.adding:
+            is_admin = instance.created_by.type == UserType.ADMIN
+            is_staff = instance.created_by.is_staff
+            has_policy = (
+                hasattr(instance.user_group, "policy_invitation_approvals")
+                and instance.user_group.policy_invitation_approvals.filter(
+                    user_type=instance.created_by.type
                 ).first()
-                if policy is not None:
-                    self.status = ApprovalStatus.PENDING
+                is not None
+            )
 
-        if old_status == ApprovalStatus.PENDING:
-            if self.status == ApprovalStatus.APPROVED:
-                # If Status changes from PENDING to APPROVED, create a new User (which should trigger an invite email),
-                # save that user to the UserGroupApprovalUserInvite.User field.
-                self.user = User.objects.create(
-                    email=self.email,
-                    username=self.email,
-                    user_group_id=self.user_group_id,
-                    type=UserType.MEMBER,
-                )
-            elif old_status == ApprovalStatus.DECLINED:
-                # If the Status changes from PENDING to DECLINED, update Status, then send email to "created_by" user with update.
-                logger.warning(
-                    f"Admin Declined Invitation of User: [{self.email}] to Company: [{self.user_group.name}]"
-                )
-                # Send email to "created_by" user with update.
-                subject = f"Admin Declined Invitation of User: [{self.email}] to Company: [{self.user_group.name}]"
-                html_content = render_to_string(
-                    "admin_approvals/emails/invite_update.min.html",
-                    {
-                        "invite_email": self.email,
-                        "first_name": self.created_by.first_name,
-                        "user_group_name": self.user_group.name,
-                        "is_approved": False,
-                    },
-                )
-                add_email_to_queue(
-                    from_email="system@trydownstream.com",
-                    to_emails=[self.created_by.email],
-                    subject=subject,
-                    html_content=html_content,
-                )
+            if is_admin or is_staff or not has_policy:
+                # If the UserGroupAdminApprovalUserInvite is being created by an ADMIN
+                # or a Staff (internal Downstream) user automatically approve the
+                # UserGroupAdminApprovalUserInvite. Also, if there is no UserGroup User
+                # Invite policy for the CreatedBy user type, automatically approve the
+                # UserGroupAdminApprovalUserInvite.
+                instance.status = ApprovalStatus.APPROVED
 
-        # Only allow changes to be made if the Status == PENDING.
-        if self.old_value("status") == ApprovalStatus.PENDING:
-            super().save(*args, **kwargs)
+        if instance.status == ApprovalStatus.APPROVED:
+            # If Status changes from PENDING to APPROVED (or was set to APPROVED above),
+            # create a new User (which should trigger an invite email from Auth0),
+            # save that user to the UserGroupApprovalUserInvite.User field.
+            instance.user = User.objects.create(
+                email=instance.email,
+                username=instance.email,
+                user_group=instance.user_group,
+                type=instance.type,
+            )
+        elif old_status == ApprovalStatus.DECLINED:
+            # If the Status changes from PENDING to DECLINED, update Status, then send email to "created_by" user with update.
+            logger.warning(
+                f"Admin Declined Invitation of User: [{instance.email}] to Company: [{instance.user_group.name}]"
+            )
+            # Send email to "created_by" user with update.
+            subject = f"Admin Declined Invitation of User: [{instance.email}] to Company: [{instance.user_group.name}]"
+            html_content = render_to_string(
+                "admin_approvals/emails/invite_update.min.html",
+                {
+                    "invite_email": instance.email,
+                    "first_name": instance.created_by.first_name,
+                    "user_group_name": instance.user_group.name,
+                    "is_approved": False,
+                },
+            )
+            add_email_to_queue(
+                from_email="system@trydownstream.com",
+                to_emails=[instance.created_by.email],
+                subject=subject,
+                html_content=html_content,
+            )
