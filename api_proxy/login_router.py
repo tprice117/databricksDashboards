af630a8886fefbb09681d84a0d9ecc40a3273f40
wickeym <wickeym@gmail.com>
2024-06-19 14:48:58 -0400

    UPDATE: Add user_id in session when invitee lands on invite link

--- api_proxy/login_router.py
+++ api_proxy/login_router.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse, HttpRequest
 from django.shortcuts import redirect, render
 from django.urls import reverse
+from django.conf import settings
 from api.models import User
 from api.utils.utils import decrypt_string
 from api.utils.auth0 import get_password_change_url
@@ -41,6 +42,28 @@ def login_view(request):
         return render(request, "admin/login.html")
 
 
+def login_redirect_view(request: HttpRequest):
+    """Auth0 Tenant Login URI points here. Redirect to the correct page after login."""
+    user_id = request.session.get("user_id", None)
+    if request.user.is_anonymous:
+        logger.info(
+            f"User is anonymous: {request} headers:[{request.headers}], query_params:[{query_params}], post_params:[{post_params}], cookies:[{request.COOKIES}], user_id:[{user_id}]"
+        )
+        if user_id:
+            user = User.objects.get(id=user_id)
+            if user.redirect_url:
+                return redirect(user.redirect_url)
+    else:
+        logger.info(
+            f"User is authenticated: {request.user.redirect_url}-{request} headers:[{request.headers}], query_params:[{query_params}], post_params:[{post_params}], cookies:[{request.COOKIES}], user_id:[{user_id}]"
+        )
+        if request.user.redirect_url:
+            # TODO: Test this, but maybe it redirect_url should be deleted after use, so that subsequent
+            # logins don't perform this, allowing the user to return to the last page they were on, on login.
+            return redirect(request.user.redirect_url)
+    return redirect(settings.BASE_URL)
+
+
 def register_account_view(request: HttpRequest):
     key = request.GET.get("key", "")
     try:
@@ -49,7 +72,9 @@ def register_account_view(request: HttpRequest):
         if user.user_id is not None:
             password_change_url = get_password_change_url(user.user_id)
             payload = {"url": password_change_url}
-            return render(request, "user-invite-email.html", payload)
+            response = render(request, "user-invite-email.html", payload)
+            request.session["user_id"] = str(user.id)
+            return response
         else:
             raise Exception("User ID is None")
     except Exception as e:
