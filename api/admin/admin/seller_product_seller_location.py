8d9fe29d8c86a556a314521c75b3f7cced6111da
wickeym <wickeym@gmail.com>
2024-07-19 09:37:29 -0400

    UPDATE: Add filter to see missing rental, one_step, and multi_step foreign key objects

--- api/admin/admin/seller_product_seller_location.py
+++ api/admin/admin/seller_product_seller_location.py
@@ -14,6 +14,56 @@ from api.forms import CsvImportForm
 from api.models import SellerLocation, SellerProduct, SellerProductSellerLocation
 
 
+class RentalModeFilter(admin.SimpleListFilter):
+    title = "rental mode"
+    parameter_name = "rental_mode"
+
+    def lookups(self, request, model_admin):
+        """
+        Returns a list of tuples. The first element in each tuple is the coded value
+        for the option that will appear in the URL query. The second element is the
+        human-readable name for the option that will appear in the right sidebar.
+        """
+        return (
+            ("rental_one_step", "Rental One Step"),
+            ("rental_one_step_missing", "Rental One Step Missing"),
+            ("rental", "Rental OG"),
+            ("rental_missing", "Rental OG Missing"),
+            ("rental_multi_step", "Rental Multi Step"),
+            ("rental_multi_step_missing", "Rental Multi Step Missing"),
+        )
+
+    def queryset(self, request, queryset):
+        """
+        Returns the filtered queryset based on the value provided in the query string
+        and retrievable via `self.value()`.
+        """
+        if self.value() == "rental_one_step":
+            return queryset.filter(
+                seller_product__product__main_product__has_rental_one_step=True
+            )
+        if self.value() == "rental_one_step_missing":
+            return queryset.filter(
+                seller_product__product__main_product__has_rental_one_step=True
+            ).filter(rental_one_step__isnull=True)
+        if self.value() == "rental":
+            return queryset.filter(
+                seller_product__product__main_product__has_rental=True
+            )
+        if self.value() == "rental_missing":
+            return queryset.filter(
+                seller_product__product__main_product__has_rental=True
+            ).filter(rental__isnull=True)
+        if self.value() == "rental_multi_step":
+            return queryset.filter(
+                seller_product__product__main_product__has_rental_multi_step=True
+            )
+        if self.value() == "rental_multi_step_missing":
+            return queryset.filter(
+                seller_product__product__main_product__has_rental_multi_step=True
+            ).filter(rental_multi_step__isnull=True)
+
+
 @admin.register(SellerProductSellerLocation)
 class SellerProductSellerLocationAdmin(admin.ModelAdmin):
     search_fields = [
@@ -26,6 +76,7 @@ class SellerProductSellerLocationAdmin(admin.ModelAdmin):
     list_filter = (
         "seller_product__product__main_product__main_product_category",
         "seller_location__seller",
+        RentalModeFilter,
     )
     inlines = [
         SellerProductSellerLocationServiceInline,
