d688cc6d57d73f54d47456e359d14782e29cfe87
apogee23 <tate.l.hayes@gmail.com>
2024-07-22 13:34:43 -0500

    improve SellerProductSellerLocationAdmin

--- common/admin/inlines/base_stacked_inline.py
+++ common/admin/inlines/base_stacked_inline.py
@@ -2,6 +2,19 @@ from django.contrib import admin
 
 
 class BaseModelStackedInline(admin.StackedInline):
+    audit_fields = [
+        "created_on",
+        "created_by",
+        "updated_on",
+        "updated_by",
+    ]
+    audit_fieldset = (
+        "Audit",
+        {
+            "fields": audit_fields,
+            "classes": ["collapse"],
+        },
+    )
     readonly_fields = [
         "created_on",
         "created_by",
