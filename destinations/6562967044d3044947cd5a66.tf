45d7279b7010726b8ad093fec9d7e7643a7e23a9
Segment Git Sync <friends@segment.com>
2024-07-30 19:18:26 +0000

    Full sync

---
+++ destinations/6562967044d3044947cd5a66.tf
@@ -0,0 +1,18 @@
+import {
+  to = segment_destination.id-6562967044d3044947cd5a66
+  id = "6562967044d3044947cd5a66"
+}
+
+resource "segment_destination" "id-6562967044d3044947cd5a66" {
+  enabled = true
+  metadata = {
+    contacts          = null
+    id                = "615c7438d93d9b61b1e9e192"
+    partner_owned     = true
+    region_endpoints  = ["EU", "US"]
+    supported_regions = ["us-west-2", "eu-west-1"]
+  }
+  name      = "Mixpanel data flow"
+  settings  = "{\"apiRegion\":\"US 🇺🇸\",\"apiSecret\":\"••••••••••ec62\",\"enabled\":false,\"name\":\"\",\"projectToken\":\"056e2004cb71d0165e243efe6026edca\",\"sourceName\":\"\",\"strictMode\":\"1\"}"
+  source_id = "uQMpUymuMWo75mypkjJoqH"
+}
