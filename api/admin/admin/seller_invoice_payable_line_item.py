6d1e61e87285231402b79ba7103c927f44d78c96
wickeym <wickeym@gmail.com>
2024-08-08 10:56:00 -0400

    FIX: Make large foreign key fields a raw_id field to speed up admin page load

--- api/admin/admin/seller_invoice_payable_line_item.py
+++ api/admin/admin/seller_invoice_payable_line_item.py
@@ -7,3 +7,9 @@ from api.models import SellerInvoicePayableLineItem
 class SellerInvoicePayableLineItemAdmin(admin.ModelAdmin):
     model = SellerInvoicePayableLineItem
     search_fields = ["id", "seller_invoice_payable__id", "order__id"]
+    raw_id_fields = (
+        "seller_invoice_payable",
+        "order",
+        "created_by",
+        "updated_by",
+    )
