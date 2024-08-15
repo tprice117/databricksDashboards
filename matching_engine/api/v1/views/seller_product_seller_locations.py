b2dabf7a69fb0e4d297b9418845abe215f5d65e8
apogee23 <tate.l.hayes@gmail.com>
2024-08-15 08:35:39 -0500

    dynamically change the SPSL object that gets returned from the MatchingEngine based on the current MainProduct configuration

--- matching_engine/api/v1/views/seller_product_seller_locations.py
+++ matching_engine/api/v1/views/seller_product_seller_locations.py
@@ -3,14 +3,12 @@ from drf_spectacular.utils import extend_schema
 from rest_framework.exceptions import APIException
 from rest_framework.views import APIView
 
+from api.models.main_product.product import Product
 from api.serializers import SellerProductSellerLocationSerializer
 from matching_engine.api.v1.serializers import MatchingEngineRequestSerializer
 from matching_engine.matching_engine import MatchingEngine
-from matching_engine.utils.align_seller_product_seller_location_children_with_main_product import (
-    align_seller_product_seller_location_children_with_main_product,
-)
-from matching_engine.utils.seller_product_seller_location_plus_take_rate import (
-    seller_product_seller_location_plus_take_rate,
+from matching_engine.utils.prep_seller_product_seller_locations_for_response import (
+    prep_seller_product_seller_locations_for_response,
 )
 
 
@@ -52,23 +50,13 @@ class GetSellerProductSellerLocationsView(APIView):
             )
         )
 
-        # Align SellerProductSellerLocations pricing configurations with
-        # current MainProduct settings.
-        seller_product_seller_locations = [
-            align_seller_product_seller_location_children_with_main_product(
-                seller_product_seller_location
-            )
-            for seller_product_seller_location in seller_product_seller_locations
-        ]
+        # Get typed Product object.
+        product: Product = serializer.validated_data["product"]
 
-        # Add default take rate to the price and serialize the data.
-        data = []
-
-        for seller_product_seller_location in seller_product_seller_locations:
-            data.append(
-                seller_product_seller_location_plus_take_rate(
-                    seller_product_seller_location,
-                )
-            )
+        # Get response data.
+        data = prep_seller_product_seller_locations_for_response(
+            main_product=product.main_product,
+            seller_product_seller_locations=seller_product_seller_locations,
+        )
 
         return JsonResponse(data, safe=False)
