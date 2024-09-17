a65502a84bcdec753f944ffe1fe9e5cb193b6f57
tprice117 <tylerprice117@gmail.com>
2024-09-17 11:03:24 -0500

    pbi Dashboard-Sales

--- dashboards/urls.py
+++ dashboards/urls.py
@@ -3,6 +3,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path("index/", views.index, name="index"),
+    path("pbiimport/", views.pbiimport, name="pbiimport"),
+
     path(
         "",
         views.command_center,
