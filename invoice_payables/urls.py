2ecb6f0170bcf955e39483724d07cb9578370741
Tyler Price <tylerprice117@gmail.com>
2024-10-10 21:38:29 -0500

    formatting

--- invoice_payables/urls.py
+++ invoice_payables/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 urlpatterns = [
     path("index/", views.index, name="index"),
-    path("<uuid:id>/", views.invoice_detail, name="invoice_detail")
+    path("<uuid:id>/", views.invoice_detail, name="invoice_detail"),
 ]
