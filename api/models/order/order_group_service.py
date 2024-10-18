031093ae36fd77be1d289b22a867f40cefbc9772
Michael Wickey <wickeym@gmail.com>
2024-10-18 09:59:03 -0400

    FIX: Add description to legacy service line item creation

--- api/models/order/order_group_service.py
+++ api/models/order/order_group_service.py
@@ -54,9 +54,13 @@ class OrderGroupService(PricingService):
 
         if not self.is_complete:
             # NOTE: This is a legacy code path until all OrderGroupService objects have price_per_mile and flat_rate_price.
+            description = "Service"
+            if self.order_group.service.miles is None:
+                description = "Flat Rate"
             return [
                 OrderLineItem(
                     order=order,
+                    description=description,
                     order_line_item_type=order_line_item_type,
                     rate=self.order_group.service.rate,
                     quantity=self.order_group.service.miles or 1,
