e3d8461f777e038eb16fcfab8a6af7607ac3be6d
Tyler Price <tylerprice117@gmail.com>
2024-10-22 10:58:24 -0500

    formatting

--- invoice_payables/forms.py
+++ invoice_payables/forms.py
@@ -4,18 +4,31 @@ from django import forms
 from django.core.exceptions import ValidationError
 from api.models import *
 
+
 class InvoiceLineItemForm(forms.Form):
-    orderlineitem_id = forms.CharField(max_length=255, widget=forms.TextInput(attrs={"class": "form-control"}))
+    orderlineitem_id = forms.CharField(
+        max_length=255, widget=forms.TextInput(attrs={"class": "form-control"})
+    )
 
     # seller_invoice_payable_line_items_id = forms.CharField(max_length=255, widget=forms.TextInput(attrs={"class": "form-control"}))
     # orderlineitemtype_id = forms.CharField(max_length=255, widget=forms.TextInput(attrs={"class": "form-control"}))
-    
-    productName= forms.CharField( max_length=255, widget=forms.TextInput(attrs={"class": "form-control"}))
-    serviceAddress = forms.CharField( max_length=255, widget=forms.TextInput(attrs={"class": "form-control"}))
-    lineItemType = forms.CharField( max_length=255, widget=forms.TextInput(attrs={"class": "form-control"}))
-    backbill = forms.BooleanField(required=False, widget=forms.CheckboxInput(attrs={"class": "form-control"}))
-    orderDate = forms.DateField( widget=forms.DateInput(attrs={"class": "form-control"}))
-    orderRate = forms.DecimalField( widget=forms.NumberInput(attrs={"class": "form-control"}))
-    orderQuantity = forms.DecimalField( widget=forms.NumberInput(attrs={"class": "form-control"}))
 
-    
+    productName = forms.CharField(
+        max_length=255, widget=forms.TextInput(attrs={"class": "form-control"})
+    )
+    serviceAddress = forms.CharField(
+        max_length=255, widget=forms.TextInput(attrs={"class": "form-control"})
+    )
+    lineItemType = forms.CharField(
+        max_length=255, widget=forms.TextInput(attrs={"class": "form-control"})
+    )
+    backbill = forms.BooleanField(
+        required=False, widget=forms.CheckboxInput(attrs={"class": "form-control"})
+    )
+    orderDate = forms.DateField(widget=forms.DateInput(attrs={"class": "form-control"}))
+    orderRate = forms.DecimalField(
+        widget=forms.NumberInput(attrs={"class": "form-control"})
+    )
+    orderQuantity = forms.DecimalField(
+        widget=forms.NumberInput(attrs={"class": "form-control"})
+    )
--- invoice_payables/views.py
+++ invoice_payables/views.py
@@ -3,7 +3,9 @@ from api.models import *
 from django.db.models import F
 import logging
 from .forms import *
+
 logger = logging.getLogger(__name__)
+
 
 # Create your views here.
 def index(request):
@@ -17,16 +19,16 @@ def index(request):
 def invoice_detail(request, id):
     context = {}
     seller_invoice_payable = SellerInvoicePayable.objects.get(pk=id)
-    
+
     line_items = SellerInvoicePayableLineItem.objects.filter(
         seller_invoice_payable=seller_invoice_payable
     )
-    
+
     # Requery the data
     queryset = (
         Order.objects.filter(seller_invoice_payable_line_items__in=line_items)
         .select_related(
-            "order_group__user_address", 
+            "order_group__user_address",
             "order_group__sellerproductsellerlocation__seller_product__product",
         )
         .annotate(
@@ -34,7 +36,9 @@ def invoice_detail(request, id):
             # orderlineitemtype_id=F("order_line_items__order_line_item_type_id"),
             service_address=F("order_group__user_address__name"),
             line_item_type=F("order_line_items__order_line_item_type__name"),
-            product_name=F("order_group__seller_product_seller_location__seller_product__product__main_product__name"),
+            product_name=F(
+                "order_group__seller_product_seller_location__seller_product__product__main_product__name"
+            ),
             backbill=F("order_line_items__backbill"),
             rate=F("order_line_items__rate"),
             quantity=F("order_line_items__quantity"),
@@ -55,13 +59,15 @@ def invoice_detail(request, id):
     )
 
     if request.method == "POST":
-        form_prefix = request.POST.get('form_prefix')
+        form_prefix = request.POST.get("form_prefix")
         form = InvoiceLineItemForm(request.POST, prefix=form_prefix)
         if form.is_valid():
             orderlineitem_id = form.cleaned_data["orderlineitem_id"]
             # seller_invoice_payable_line_items_id = form.cleaned_data["seller_invoice_payable_line_items_id"]
             orderlineitem_obj = Order.objects.get(pk=orderlineitem_id)
-            seller_invoice_payable_line_item_obj = SellerInvoicePayableLineItem.objects.get(pk=orderlineitem_id)
+            seller_invoice_payable_line_item_obj = (
+                SellerInvoicePayableLineItem.objects.get(pk=orderlineitem_id)
+            )
             # orderlineitemtype_id = OrderLineItem.objects.get(pk=orderlineitem_id)
             orderlineitem_obj.product_name = form.cleaned_data["productName"]
             orderlineitem_obj.service_address = form.cleaned_data["serviceAddress"]
@@ -75,7 +81,7 @@ def invoice_detail(request, id):
             print(f"Updated order line item {orderlineitem_id}")
         else:
             print(f"Form is not valid: {form.errors}")
-        return redirect('invoice_detail', id=id)  
+        return redirect("invoice_detail", id=id)
     # Map line items to their orders for use in the template
     line_item_order_map = {item: item.order for item in line_items}
 
@@ -96,18 +102,24 @@ def invoice_detail(request, id):
     context["line_items"] = line_items
     context["related_orders"] = queryset
     context["line_item_order_map"] = line_item_order_map
-    context["forms"] = [InvoiceLineItemForm(initial={
-        "orderlineitem_id": orderlineitem["id"],
-        # "seller_invoice_payable_line_items_id": orderlineitem["seller_invoice_payable_line_items_id"],
-        # "orderlineitemtype_id": orderlineitem["orderlineitemtype_id"],
-        "productName": orderlineitem["product_name"],
-        "serviceAddress": orderlineitem["service_address"],
-        "lineItemType": orderlineitem["line_item_type"],
-        "backbill": orderlineitem["backbill"],
-        "orderDate": orderlineitem["order_date"],
-        "orderRate": orderlineitem["rate"],
-        "orderQuantity": orderlineitem["quantity"],
-    }, prefix=str(orderlineitem["id"])) for orderlineitem in queryset]
+    context["forms"] = [
+        InvoiceLineItemForm(
+            initial={
+                "orderlineitem_id": orderlineitem["id"],
+                # "seller_invoice_payable_line_items_id": orderlineitem["seller_invoice_payable_line_items_id"],
+                # "orderlineitemtype_id": orderlineitem["orderlineitemtype_id"],
+                "productName": orderlineitem["product_name"],
+                "serviceAddress": orderlineitem["service_address"],
+                "lineItemType": orderlineitem["line_item_type"],
+                "backbill": orderlineitem["backbill"],
+                "orderDate": orderlineitem["order_date"],
+                "orderRate": orderlineitem["rate"],
+                "orderQuantity": orderlineitem["quantity"],
+            },
+            prefix=str(orderlineitem["id"]),
+        )
+        for orderlineitem in queryset
+    ]
 
     context["queryset"] = queryset
     return render(request, "invoice_payables/invoice_detail.html", context)
