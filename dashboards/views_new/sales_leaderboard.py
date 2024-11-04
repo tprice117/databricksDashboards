c518cf00e8329574bf011a49881381b412c4a0e6
Tyler Price <tylerprice117@gmail.com>
2024-11-04 11:13:43 -0600

    sales-dashboard Updates

--- dashboards/views_new/sales_leaderboard.py
+++ dashboards/views_new/sales_leaderboard.py
@@ -8,31 +8,31 @@ logger = logging.getLogger(__name__)
 
 from django.shortcuts import render
 from django.utils import timezone
+from django.db.models import Count
 
 from api.models.order.order import *
 from api.models.order.order_group import *
 from api.models.order.order_line_item import *
 from api.models.seller.seller import *
 from api.models.user.user_group import *
+from django.db.models import Sum, F
 
+# Define first_of_month and orders_this_month outside the views
+first_of_month = timezone.now().replace(
+    day=1,
+    hour=0,
+    minute=0,
+    second=0,
+    microsecond=0,
+)
+
+orders_this_month = Order.objects.filter(
+    end_date__gte=first_of_month,
+    status=Order.Status.COMPLETE,
+)
 
 @login_required(login_url="/admin/login/")
 def sales_leaderboard(request):
-    # Get the first of the month.
-    first_of_month = timezone.now().replace(
-        day=1,
-        hour=0,
-        minute=0,
-        second=0,
-        microsecond=0,
-    )
-
-    # Get Orders for the current month.
-    orders_this_month = Order.objects.filter(
-        end_date__gte=first_of_month,
-        status=Order.Status.COMPLETE,
-    )
-
     # Get all Users.
     users = User.objects.filter(
         is_staff=True,
@@ -94,21 +94,6 @@ def user_sales_detail(request, user_id):
     except User.DoesNotExist:
         return render(request, "404.html", status=404)
 
-    # Get the first of the month.
-    first_of_month = timezone.now().replace(
-        day=1,
-        hour=0,
-        minute=0,
-        second=0,
-        microsecond=0,
-    )
-
-    # Get Orders for the current month for the specific user.
-    orders_this_month = Order.objects.filter(
-        end_date__gte=first_of_month,
-        status=Order.Status.COMPLETE,
-        order_group__user_address__user_group__account_owner=user,
-    )
     orders_for_user = orders_this_month.filter(
         order_group__user_address__user_group__account_owner=user,
     )
@@ -118,3 +103,37 @@ def user_sales_detail(request, user_id):
     context["ordersforuser"] = orders_for_user
     context["order_count"] = order_count
     return render(request, "dashboards/user_sales_detail.html", context)
+
+def user_sales_product_mix(request, user_id):
+    context = {}
+    try:
+        user = User.objects.get(id=user_id, is_staff=True, groups__name="Sales")
+    except User.DoesNotExist:
+        return render(request, "404.html", status=404)
+
+    orders_for_user = orders_this_month.filter(
+        order_group__user_address__user_group__account_owner=user,
+    )
+    # Group orders by product name.
+    product_sales = orders_for_user.values(
+        main_product_name=F('order_group__seller_product_seller_location__seller_product__product__main_product__name')
+    ).annotate(
+        order_count=Count('id')
+    ).order_by('order_count')
+
+    context["product_sales"] = product_sales
+    context["user"] = user
+    context["orders_this_month"] = orders_this_month
+    return render(request, "dashboards/user_sales_product_mix.html", context)
+
+def user_sales_top_accounts(request, user_id):
+    context = {}
+    try:
+        user = User.objects.get(id=user_id, is_staff=True, groups__name="Sales")
+    except User.DoesNotExist:
+        return render(request, "404.html", status=404)
+    orders_for_user = orders_this_month.filter(
+        order_group__user_address__user_group__account_owner=user,
+    )
+    context["user"] = user
+    return render(request, "dashboards/user_sales_top_accounts.html", context)
