af70e56c0e75b096076be534909655fedf332182
Tyler Price <tylerprice117@gmail.com>
2024-11-12 10:27:50 -0600

    28 day list dashboard

--- dashboards/views_new/sales_leaderboard.py
+++ dashboards/views_new/sales_leaderboard.py
@@ -206,7 +206,36 @@ def user_sales_churned_accounts(request, user_id):
     return render(request, "dashboards/user_sales_churned_accounts.html", context)
 
 def user_sales_28_day_list(request, user_id):
-    return render(request, "dashboards/user_sales_28_day_list.html")
+    context = {}
+    try:
+        user = User.objects.get(id=user_id, is_staff=True, groups__name="Sales")
+    except User.DoesNotExist:
+        return render(request, "404.html", status=404)
+    orders_for_user = Order.objects.filter(
+        order_group__user_address__user_group__account_owner=user,
+        end_date__gte=timezone.now() - timezone.timedelta(days=28),
+    ).annotate(
+        annotated_order_group_id=F("order_group__id"),
+        order_group_end_date=F("order_group__end_date"),
+        order_end_date=F("end_date"),
+        order_id=F("id"),
+        user_account_owner=F("order_group__user_address__user_group__account_owner__username"),
+        user_address_name=F("order_group__user_address__name"),
+    )
+    context["orders_for_user"] = orders_for_user
+
+
+
+    context["user"] = user
+    return render(request, "dashboards/user_sales_28_day_list.html", context)
 
 def user_sales_new_buyers(request, user_id):
-    return render(request, "dashboards/user_sales_new_buyers.html")
+    context = {}
+    try:
+        user = User.objects.get(id=user_id, is_staff=True, groups__name="Sales")
+    except User.DoesNotExist:
+        return render(request, "404.html", status=404)
+    
+
+    context["user"] = user
+    return render(request, "dashboards/user_sales_new_buyers.html", context)
