d9c647761c68605cb5588fc0a2c8ad56cc22de0d
Michael Wickey <wickeym@gmail.com>
2024-11-19 11:09:37 -0500

    UPDATE: Add universal import/export

--- api/admin/admin/payout.py
+++ api/admin/admin/payout.py
@@ -1,10 +1,20 @@
 from django.contrib import admin
+from import_export.admin import ExportActionMixin
+from import_export import resources
 
 from api.models import Payout
+from common.admin.admin.base_admin import BaseModelAdmin
+
+
+class PayoutResource(resources.ModelResource):
+    class Meta:
+        model = Payout
+        skip_unchanged = True
 
 
 @admin.register(Payout)
-class PayoutAdmin(admin.ModelAdmin):
+class PayoutAdmin(BaseModelAdmin, ExportActionMixin):
+    resource_classes = [PayoutResource]
     model = Payout
     list_display = (
         "id",
@@ -19,3 +29,20 @@ class PayoutAdmin(admin.ModelAdmin):
     search_fields = ["id", "lob_check_id", "checkbook_payout_id", "stripe_transfer_id"]
     raw_id_fields = ("order", "created_by", "updated_by")
     list_filter = ("created_on",)
+    fieldsets = [
+        (
+            None,
+            {
+                "fields": [
+                    "order",
+                    "checkbook_payout_id",
+                    "check_number",
+                    "stripe_transfer_id",
+                    "lob_check_id",
+                    "amount",
+                    "description",
+                ]
+            },
+        ),
+        BaseModelAdmin.audit_fieldset,
+    ]
