3ca509b5f78d2b46eee2c38a80837a3b1b38b489
David Allen <dawork20100@gmail.com>
2024-12-03 14:05:11 -0600

    fix: restore get_file_path args

--- common/utils/get_file_path.py
+++ common/utils/get_file_path.py
@@ -1,7 +1,7 @@
 import uuid
 
 
-def get_file_path(filename):
+def get_file_path(instance, filename):
     ext = filename.split(".")[-1]
     randomized_filename = "%s.%s" % (uuid.uuid4(), ext)
     return randomized_filename
