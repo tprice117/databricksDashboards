52ac3777c0d279e3c6f1edac972c964b26e617f1
David Allen <dawork20100@gmail.com>
2024-12-10 12:59:36 -0600

    feat: schedule supplier remittance email

--- billing/scheduled_jobs/supplier_remittance.py
+++ billing/scheduled_jobs/supplier_remittance.py
@@ -11,8 +11,13 @@ TEMPLATE_ID = 13
 def calculate_last_saturday():
     """Calculate the date of the last Saturday."""
     today = timezone.now().date()
-    last_saturday = today - timezone.timedelta(days=(today.weekday() + 2) % 7)
-    return last_saturday
+    weekday = (today.weekday() + 2) % 7
+
+    if weekday == 0:
+        # Return last Saturday if today is Saturday.
+        return today - timezone.timedelta(days=7)
+
+    return today - timezone.timedelta(days=weekday)
 
 
 def get_grouped_payouts(start_date, end_date):
@@ -20,7 +25,6 @@ def get_grouped_payouts(start_date, end_date):
     return (
         Payout.objects.select_related(
             "order__order_group__seller_product_seller_location__seller_location",
-            "order__order_group__seller_product_seller_location__seller_location__seller__usergroup",
         )
         .filter(Q(created_on__date__gte=start_date) & Q(created_on__date__lte=end_date))
         .values(
@@ -31,7 +35,10 @@ def get_grouped_payouts(start_date, end_date):
 
 
 def get_seller_location_with_billing_users(seller_location_id):
-    """Fetch SellerLocation with prefetching billing users."""
+    """
+    Fetch SellerLocation with prefetching billing users.
+    For now, users do not have associated SellerLocations, so we are fetching all billing users.
+    """
     return SellerLocation.objects.prefetch_related(
         Prefetch(
             "seller__usergroup__users",
@@ -41,13 +48,18 @@ def get_seller_location_with_billing_users(seller_location_id):
 
 
 def send_supplier_remittance_emails():
-    """Notifying suppliers of their payouts that were submitted this week for the related payout period.
+    """
+    Notifying suppliers of their payouts that were submitted this week for the related payout period.
     Trigger Logic:  Every Friday when a Seller has a related payout.created_on between last Saturday and today
     """
     # Get all Payouts since last Saturday.
     last_saturday = calculate_last_saturday()
     today = timezone.now().date()
     grouped_payouts = get_grouped_payouts(last_saturday, today)
+
+    if not grouped_payouts.exists():
+        # No payouts between now and last Saturday.
+        return
 
     for payout in grouped_payouts:
         seller_location = get_seller_location_with_billing_users(
@@ -56,23 +68,24 @@ def send_supplier_remittance_emails():
             ],
         )
 
-        emails = seller_location.seller.usergroup.users.values_list(
-            "email", flat=True
-        ).distinct()
-        emails.append(seller_location.order_email)
+        emails = [seller_location.order_email]
+
+        if hasattr(seller_location.seller, "usergroup"):
+            emails_query = seller_location.seller.usergroup.users.values_list(
+                "email", flat=True
+            )
+            emails.extend([email for email in emails_query])
 
         message_data = {
-            "payout_total": payout["total_amount"],  # payout_total
+            "payout_total": f"{payout['total_amount']:.2f}",  # payout_total
             "seller_location_name": seller_location.name,  # seller_location_name
-            "seller_location_mailing_address": seller_location.formatted_address,  # seller_location_mailing_address
-            "user_group_name": seller_location.seller.usergroup.name
-            if hasattr(seller_location.seller, "usergroup")
-            else seller_location.seller.name,  # user_group_name
+            "seller_location_mailing_address": seller_location.mailing_address.formatted_address,  # seller_location_mailing_address
+            "user_group_name": seller_location.seller.name,  # user_group_name
         }
 
         customerio.send_email(
-            emails=emails,
+            send_to=emails,
             message_data=message_data,
-            subject=f"{message_data["user_group_name"]}'s Weekly Payout Remittance from Downstream Marketplace",
+            subject=f"{message_data['user_group_name']}'s Weekly Payout Remittance from Downstream Marketplace",
             template_id=TEMPLATE_ID,
         )
