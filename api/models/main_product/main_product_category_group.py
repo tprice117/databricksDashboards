2fb6ec69cac3093bff7911bedd55c5cd90cd457a
jared <jaredm.stutzman@gmail.com>
2024-12-26 16:27:52 -0500

    add icon field to MainProductCategoryGroup

--- api/models/main_product/main_product_category_group.py
+++ api/models/main_product/main_product_category_group.py
@@ -1,11 +1,13 @@
 from django.db import models
 
 from common.models import BaseModel
+from common.utils.get_file_path import get_file_path
 
 
 class MainProductCategoryGroup(BaseModel):
     name = models.CharField(max_length=80)
     sort = models.IntegerField()
+    icon = models.ImageField(upload_to=get_file_path, blank=True, null=True)
 
     def __str__(self):
         return self.name
