ec24f800272f11a32d69c45a7e239f53664e3ae7
Michael Wickey <wickeym@gmail.com>
2025-01-09 11:06:03 -0500

    FIX: Get UserAddress from stripe_customer_id since user_group_id is not in metadata

--- billing/scheduled_jobs/attempt_charge_for_past_due_invoices.py
+++ billing/scheduled_jobs/attempt_charge_for_past_due_invoices.py
@@ -1,6 +1,7 @@
 import datetime
+from django.utils import timezone
 
-from api.models import UserGroup
+from api.models import UserAddress, UserGroup
 from common.utils.stripe.stripe_utils import StripeUtils
 import logging
 
@@ -28,22 +29,46 @@ def attempt_charge_for_past_due_invoices():
     invoices_due = []
     for invoice in all_open_invoices:
         # Get UserGroup for this invoice.
-        user_group_id = invoice["metadata"].get("user_group_id")
+        user_address = UserAddress.objects.filter(
+            stripe_customer_id=invoice["customer"],
+        ).first()
+        user_group = None
+        if user_address:
+            user_group = user_address.user_group
+        else:
+            invoices_due.append(invoice)
+            logger.error(
+                f"attempt_charge_for_past_due_invoices: stripe_customer_id: {invoice['customer']} not found for invoice {invoice['id']}"
+            )
+            continue
         # Check if invoice is due.
-        if user_group_id and invoice["due_date"]:
-            user_group = UserGroup.objects.filter(id=user_group_id).first()
-            if not user_group:
-                logger.error(
-                    f"attempt_charge_for_past_due_invoices: UserGroup: {user_group_id} not found for invoice {invoice['id']}"
-                )
-                continue
+        if user_group and invoice["due_date"]:
             if user_group.net_terms == UserGroup.NetTerms.IMMEDIATELY:
+                # due_date = (
+                #     timezone.make_aware(
+                #         timezone.datetime.fromtimestamp(invoice["due_date"]),
+                #         timezone.get_current_timezone(),
+                #     )
+                #     if invoice["due_date"]
+                #     else None
+                # )
+                # print(f"1 customer: {user_group.name} invoice due_date: {due_date}")
                 invoices_due.append(invoice)
             elif invoice["due_date"] < two_weeks_ago.timestamp():
                 # Give a 2 week grace period for net terms customers since we allow payment by check.
                 # This allows for the check to be mailed and received by us before attempting to charge the card.
                 invoices_due.append(invoice)
         else:
+            # due_date = (
+            #     timezone.make_aware(
+            #         timezone.datetime.fromtimestamp(invoice["due_date"]),
+            #         timezone.get_current_timezone(),
+            #     )
+            #     if invoice["due_date"]
+            #     else None
+            # )
+            # if user_address and not user_group:
+            #     print(f"2 customer: {user_address.name} invoice due_date: {due_date}")
             # If no due date (or no UserGroup), then we assume it is due immediately.
             invoices_due.append(invoice)
 
