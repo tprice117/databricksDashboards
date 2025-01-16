23a218dfc970723c3f4e61428e607e16f5184f25
David Allen <dawork20100@gmail.com>
2025-01-16 14:04:05 -0600

    feat: automatically assign owner to new leads

--- api/managers/__init__.py
+++ api/managers/__init__.py
@@ -1,5 +1,5 @@
 from .seller_product_seller_location import SellerProductSellerLocationManager
-from .user import CustomerTeamManager
+from .user import CustomerTeamManager, SalesTeamManager
 from .user_address import UserAddressManager
 from .order import OrderManager
 from .order_group import OrderGroupManager
