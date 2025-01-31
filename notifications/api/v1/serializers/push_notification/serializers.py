78022719107fb27c49db94ee2222bec8b1a57a8c
Michael Wickey <wickeym@gmail.com>
2025-01-31 13:02:43 -0500

    FIX: Add is_read to PushNotification api reponse

--- notifications/api/v1/serializers/push_notification/serializers.py
+++ notifications/api/v1/serializers/push_notification/serializers.py
@@ -4,6 +4,15 @@ from notifications.models import PushNotification
 
 
 class PushNotificationSerializer(serializers.ModelSerializer):
+    is_read = serializers.SerializerMethodField()
+
+    def get_is_read(self, obj) -> bool:
+        return (
+            obj.push_notification_tos.filter(user=self.context["request"].user)
+            .first()
+            .is_read
+        )
+
     class Meta:
         model = PushNotification
         fields = "__all__"
