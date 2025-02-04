c828e80c2b4e2c699865c947e16d5f4d6c962c35
Tyler Price <tylerprice117@gmail.com>
2025-02-04 16:01:25 -0600

    visibilty changes for the table

--- api_proxy/settings.py
+++ api_proxy/settings.py
@@ -30,11 +30,10 @@ environ.Env.read_env(BASE_DIR / ".env")
 
 SECRET_KEY = env("DJANGO_SECRET_KEY")
 
-ENVIRONMENT = os.getenv("ENV")
-DEBUG = os.getenv("ENV") != "TEST"
+ENVIRONMENT = "TEST"
 # NOTE: DRF Standardized Errors will handle uncaught exceptions if this is set to False.
 # Setting it to True will still show the Django error page on uncaught exceptions.
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
