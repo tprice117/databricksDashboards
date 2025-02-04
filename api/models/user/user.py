b6e05602ba99d8c0f8a1fa3418a05e13569af8de
jared <jaredm.stutzman@gmail.com>
2025-02-04 16:57:42 -0500

    new company adopts user address

--- api/models/user/user.py
+++ api/models/user/user.py
@@ -39,6 +39,7 @@ mailchimp = MailchimpTransactional.Client(settings.MAILCHIMP_API_KEY)
     "salesforce_contact_id",
     "salesforce_seller_location_id",
     "terms_accepted",
+    "user_group",
 )
 class User(AbstractUser):
     class ApolloStage(models.TextChoices):
@@ -436,6 +437,20 @@ def user_post_save(sender, instance, created: bool, **kwargs):
             logger.info(f"New lead created: {lead}")
         except Exception as e:
             logger.error(f"Error creating new lead: {e}", exc_info=e)
+    # user_address must connect to the same user_group as the user is
+    if instance.old_value("user_group") is None:
+        if instance.user_group:
+            user_address = instance.useraddress_set.first()
+            if user_address:
+                user_address.user_group = instance.user_group
+                user_address.save()
+    else:
+        if instance.user_group is None:
+            user_address = instance.useraddress_set.first()
+            # disconnect user_address from user
+            if user_address:
+                user_address.user = None
+                user_address.save()
 
 
 class CompanyUtils:
