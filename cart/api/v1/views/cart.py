42e661618e638afc6848b297be33c047d320f00a
Michael Wickey <wickeym@gmail.com>
2025-02-05 16:35:16 -0500

    FIX: Add select/prefetch related to cart api for optimization

--- cart/api/v1/views/cart.py
+++ cart/api/v1/views/cart.py
@@ -21,6 +21,13 @@ class CartView(APIView):
           A Cart containing a list of CartGroups of CartItems.
         """
         orders = CartUtils.get_booking_objects(request)
+        orders = orders.filter(submitted_on__isnull=False).select_related(
+            "order_group__seller_product_seller_location__seller_product__seller",
+            "order_group__user_address",
+            "order_group__user",
+            "order_group__seller_product_seller_location__seller_product__product__main_product",
+        )
+        orders = orders.prefetch_related("payouts", "order_line_items")
         cart_data = CartUtils.get_cart_orders(orders)
 
         data = CartSerializer(cart_data).data
