931190252ace8a1877ab8799a77848124ced15ec
David Allen <dawork20100@gmail.com>
2025-02-12 16:40:34 -0600

    refactor: open intercom, add file ext to attachment img

--- customer_dashboard/templatetags/sales_stream_tags.py
+++ customer_dashboard/templatetags/sales_stream_tags.py
@@ -58,10 +58,13 @@ def downstream_naturaltime(value):
 
 
 @register.filter(is_safe=True)
-def render_file(file, width=100, height=100):
-    """Render an image file"""
-    image_extensions = [".png", ".jpg", ".jpeg", ".gif", ".svg", ".bmp", ".webp"]
+def render_file(file):
+    """Render an image file. `width` and `height` are optional, expect value in pixels."""
+    image_extensions = [".png", ".jpg", ".jpeg", ".gif", ".bmp", ".webp"]
+    # Need a better way to ensure image size is correct.
     if any(file.name.lower().endswith(ext) for ext in image_extensions):
-        return f'<img src="{file.url}" alt="{file.name}" style="width: {width}px; height: {height}px; object-fit: contain;">'
+        style = "object-fit: cover; width:150px; height:150px; max-width: 100%; max-height: 100%;"
+        return f'<img src="{file.url}" alt="{file.name}" style="{style}">'
     else:
-        return f'<i class="fas fa-clipboard" style="font-size: {width}px; height: {height}px;"></i>'
+        style = "object-fit: contain; font-size: 150px; width: 100%; height: 100%;"
+        return f'<i class="fas fa-file position-relative" style="{style}"><p class="position-absolute text-white fs-3" style="top: 50%; left: 50%; transform: translate(-50%, -50%);">.{file.name.split(".")[-1]}</p></i>'
