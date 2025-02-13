011315a07c63b3d0d85b9d27890ddcdc4f9148d2
David Allen <dawork20100@gmail.com>
2025-02-13 11:00:43 -0600

    refactor: create common note model

--- crm/models/lead_note.py
+++ crm/models/lead_note.py
@@ -1,12 +1,11 @@
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 
-from common.models import BaseModel
+from common.models import NoteModel
 from crm.models.lead import Lead
 
 
-class LeadNote(BaseModel):
+class LeadNote(NoteModel):
     """A model for capturing notes/comments about a particular lead."""
 
-    text = models.TextField(_("Note Text"))
     lead = models.ForeignKey(Lead, on_delete=models.CASCADE, related_name="notes")
