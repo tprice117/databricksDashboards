7574d75edb697fec7088c1bb0bfa5ae855a76eff
Michael Wickey <wickeym@gmail.com>
2025-02-13 16:57:32 -0500

    FIX: Use raw id field in UserGroupAdminApprovalUserInviteInline

--- admin_approvals/admin/inlines/user_group_admin_approval_user_invite.py
+++ admin_approvals/admin/inlines/user_group_admin_approval_user_invite.py
@@ -6,5 +6,6 @@ from admin_approvals.models import UserGroupAdminApprovalUserInvite
 class UserGroupAdminApprovalUserInviteInline(admin.TabularInline):
     model = UserGroupAdminApprovalUserInvite
     fields = ("user_group", "user", "email", "status")
+    raw_id_fields = ("user_group", "user")
     show_change_link = False
     extra = 0
