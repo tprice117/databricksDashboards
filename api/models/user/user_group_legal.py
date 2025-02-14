b8a6d93953535fe56e282d9821f95ab7f42a01d9
Michael Wickey <wickeym@gmail.com>
2025-02-14 08:05:52 -0500

    UPDATE: Add formatted_address property to UserGroupLegal

--- api/models/user/user_group_legal.py
+++ api/models/user/user_group_legal.py
@@ -39,6 +39,10 @@ class UserGroupLegal(BaseModel):
     latitude = models.DecimalField(max_digits=18, decimal_places=15, blank=True)
     longitude = models.DecimalField(max_digits=18, decimal_places=15, blank=True)
 
+    @property
+    def formatted_address(self):
+        return f"{self.street} {self.city}, {self.state} {self.postal_code}"
+
     def pre_save(sender, instance, *args, **kwargs):
         latitude, longitude = geocode_address(
             f"{instance.street} {instance.city} {instance.state} {instance.postal_code}"
