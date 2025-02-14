a06cdd4a540181f7b699114726dd8f01e85e93a8
jared <jaredm.stutzman@gmail.com>
2025-02-14 10:31:18 -0500

    customer_rate_decimal keep it separate

--- api/admin/inlines/order/order_items/fees/order_material_fee.py
+++ api/admin/inlines/order/order_items/fees/order_material_fee.py
@@ -1,8 +1,29 @@
 from api.admin.inlines.order.order_items.common.order_item_inline import OrderItemInline
 from api.models import OrderMaterialFee
+from common.admin.inlines.base_tabular_inline import BaseModelTabularInline
 
 
 class OrderMaterialFeeInline(OrderItemInline):
     model = OrderMaterialFee
     show_change_link = True
+    fields = [
+        "quantity_decimal",
+        "customer_rate_decimal",
+        "seller_rate_decimal",
+        "customer_price",
+        "seller_price",
+        "platform_fee",
+        "platform_fee_percent",
+        "description",
+        "stripe_invoice_line_item_id",
+        "paid",
+    ] + BaseModelTabularInline.audit_fields
+    readonly_fields = [
+        "customer_price",
+        "seller_price",
+        "platform_fee",
+        "platform_fee_percent",
+        "stripe_invoice_line_item_id",
+        "paid",
+    ] + BaseModelTabularInline.readonly_fields
     extra = 0
--- api/models/order/order_items/fees/order_material_fee.py
+++ api/models/order/order_items/fees/order_material_fee.py
@@ -16,16 +16,34 @@ class OrderMaterialFee(OrderItem):
         decimal_places=4,
         help_text="The quantity of the material fee.",
     )
+    customer_rate_decimal = models.DecimalField(
+        max_digits=18,
+        decimal_places=2,
+        help_text="The rate the customer is charged for this item (ex: 25.00)",
+    )
+    seller_rate_decimal = models.DecimalField(
+        max_digits=18,
+        decimal_places=2,
+        help_text="The rate the seller is paid for this item (ex: 20.00)",
+    )
+
     # Making the 'quantity' field optional since the pre_save
     # signal will set the value of 'quantity' based on the value of
     # 'quantity_decimal'.
     quantity = models.IntegerField(
         blank=True,
     )
-
-    rate_includes_quantity = models.BooleanField(
-        default=False,
-        help_text="Whether the rate includes the quantity or needs to be multiplied by the quantity.",
+    customer_rate = models.DecimalField(
+        max_digits=18,
+        decimal_places=2,
+        blank=True,
+        help_text="The rate the customer is charged for this item (ex: 25.00)",
+    )
+    seller_rate = models.DecimalField(
+        max_digits=18,
+        decimal_places=2,
+        blank=True,
+        help_text="The rate the seller is paid for this item (ex: 20.00)",
     )
 
     class Meta:
@@ -37,22 +55,17 @@ class OrderMaterialFee(OrderItem):
 def pre_save_order_material_fee(sender, instance: OrderMaterialFee, **kwargs):
     # Since the Stripe Invoice API requires 'quantity' to be an integer, we will
     # set the quantity to 1 if the quantity_decimal is not an whole number.
-    # and price will include the quantity already.
     if not instance.quantity_decimal % 1 == 0:
         # 'quantity_decimal' is a whole number. Set 'quantity' to the 'quantity_decimal'
         # value converted to an integer.
         instance.quantity = int(instance.quantity_decimal)
-        if instance.rate_includes_quantity:
-            instance.rate_includes_quantity = False
-            # Update customer_rate and seller_rate to be the price per unit
-            instance.customer_rate = instance.customer_rate / instance.quantity_decimal
-            instance.seller_rate = instance.seller_rate / instance.quantity_decimal
     else:
         # 'quantity_decimal' is not a whole number. Set 'quantity' to 1.
         instance.quantity = 1
-        if not instance.rate_includes_quantity:
-            instance.rate_includes_quantity = True
-            # Update customer_rate and seller_rate to be the total price of all the units,
-            # instead of the price per unit.
-            instance.customer_rate = instance.customer_rate * instance.quantity_decimal
-            instance.seller_rate = instance.seller_rate * instance.quantity_decimal
+
+        # Update customer_rate and seller_rate to be the total price of the material fee,
+        # instead of the price per unit.
+        instance.customer_rate = (
+            instance.customer_rate_decimal * instance.quantity_decimal
+        )
+        instance.seller_rate = instance.seller_rate_decimal * instance.quantity_decima
