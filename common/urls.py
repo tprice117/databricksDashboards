4c2781cc2fb2d602b8413cf1d06ff53d4366b14f
Michael Wickey <wickeym@gmail.com>
2025-02-18 12:32:25 -0500

    FIX: Switch webhook urls

--- common/urls.py
+++ common/urls.py
@@ -4,6 +4,6 @@ from common.utils.stripe.webhooks import stripe_webhook
 from common.utils.customerio import customerio_webhook
 
 urlpatterns = [
-    path("webhook/stripe/", customerio_webhook),
-    path("webhook/customerio/", stripe_webhook),
+    path("webhook/stripe/", stripe_webhook),
+    path("webhook/customerio/", customerio_webhook),
 ]
